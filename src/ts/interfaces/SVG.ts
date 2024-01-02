8762920f12b9c1a33809ba12db9bd6ac394cf577
Lars van Vianen <lars@vianen.com>
2024-01-02 16:53:38 +0100

    Update SVG.ts

--- src/ts/interfaces/SVG.ts
+++ src/ts/interfaces/SVG.ts
@@ -3,10 +3,10 @@
 
 // Assuming the structure of your SVG object, you might need to adjust these types
 interface Svg {
-    metadata: {
-      name: string;
-      // ... other metadata properties
-    };
-    source: string;
-    // ... other Svg properties
-  }
+        metadata: {
+        name: string;
+        // ... other metadata properties
+        };
+        source: string;
+        // ... other Svg properties
+}
