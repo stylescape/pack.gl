15dfffd5fc79142c5ed624dcb5f51b71bb6d671f
Lars van Vianen <lars@vianen.com>
2024-07-13 12:34:26 +0200

    Update config.ts

--- src/ts/config/config.ts
+++ src/ts/config/config.ts
@@ -1,6 +1,6 @@
 // config/config.ts
 
-// Copyright 2023 Scape Agency BV
+// Copyright 2024 Scape Agency BV
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
