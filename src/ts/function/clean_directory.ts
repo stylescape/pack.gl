b9ac5a29f13480aac6ef06a09bd0e6b5e870392d
Lars van Vianen <lars@vianen.com>
2024-07-14 23:04:52 +0200

    Update clean_directory.ts

--- src/ts/function/clean_directory.ts
+++ src/ts/function/clean_directory.ts
@@ -1,4 +1,4 @@
-// config/fantasticon.config.ts
+// function/clean_directory.ts
 
 // Copyright 2023 Scape Agency BV
 
@@ -27,6 +27,7 @@ import StylizedLogger from '../class/StylizedLogger.js';
 // Constants
 // ============================================================================
 
+// Create instances of DirectoryCleaner and StylizedLogger
 const directoryCleaner = new DirectoryCleaner();
 const logger = new StylizedLogger();
 
@@ -36,8 +37,11 @@ const logger = new StylizedLogger();
 // ============================================================================
 
 /**
- * Cleans a specified directory and logs the process.
- * @param directoryPath - The path of the directory to clean.
+ * Cleans the specified directory and logs the operation.
+ * This function is asynchronous and will log details about the cleaning process,
+ * including any errors that occur.
+ * 
+ * @param directoryPath - The file system path to the directory to be cleaned.
  */
 async function cleanDirectory(directoryPath: string): Promise<void> {
     try {
@@ -58,7 +62,10 @@ async function cleanDirectory(directoryPath: string): Promise<void> {
 export default cleanDirectory;
 
 
-// Usage example
-// cleanAndLogDirectory(CONFIG.path.dist)
-//     .then(() => console.log('Directory cleaning completed.'))
-//     .catch(error => console.error(error));
+// ============================================================================
+// Example
+// ============================================================================
+
+// cleanDirectory('path/to/directory')
+//     .then(() => console.log('Directory cleaning completed successfully.'))
+//     .catch(error => console.error('Directory cleaning failed:', error));
