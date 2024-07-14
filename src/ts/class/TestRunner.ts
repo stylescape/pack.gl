eaea8d61210e65b5e622dfaf3c3e61b626027d45
Lars van Vianen <lars@vianen.com>
2024-07-14 23:05:35 +0200

    Update TestRunner.ts

--- src/ts/class/TestRunner.ts
+++ src/ts/class/TestRunner.ts
@@ -29,16 +29,28 @@ const execAsync = util.promisify(exec);
 // Classes
 // ============================================================================
 
+/**
+ * A utility class for running tests from Node.js applications. It simplifies the process of
+ * executing shell commands for testing by providing a wrapper around the child_process module.
+ */
 class TestRunner {
+
     private testCommand: string;
 
+    /**
+     * Constructs a TestRunner with a specific test command.
+     * @param {string} testCommand - The command used to run tests (e.g., 'npm test').
+     */
     constructor(testCommand: string) {
         this.testCommand = testCommand;
     }
 
     /**
-     * Runs the tests using the specified test command.
-     * @returns A promise that resolves with the test results.
+     * Executes the test command in a child process and returns the results.
+     * This method captures both stdout and stderr streams, handling any errors by re-throwing them.
+     *
+     * @returns {Promise<string>} A promise that resolves with the test results as a string if successful.
+     * @throws {Error} Throws an error if the test command fails or if stderr captures any error output.
      */
     async runTests(): Promise<string> {
         try {
@@ -64,14 +76,13 @@ class TestRunner {
 export default TestRunner;
 
 
-
-// Usage Example
-// Here's an example of how you might use the TestRunner class in a project:
+// ============================================================================
+// Example
+// ============================================================================
 
 // import TestRunner from './TestRunner';
 
 // const runner = new TestRunner('npm test'); // Replace 'npm test' with your actual test command
-
 // runner.runTests()
 //     .then(results => {
 //         console.log('Test Results:', results);
