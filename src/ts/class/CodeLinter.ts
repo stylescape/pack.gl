b11194a566677f37fe5ac25fa8448681493546c4
Lars van Vianen <lars@vianen.com>
2024-07-14 23:03:21 +0200

    Update CodeLinter.ts

--- src/ts/class/CodeLinter.ts
+++ src/ts/class/CodeLinter.ts
@@ -19,7 +19,6 @@
 // Import
 // ============================================================================
 
-
 import { ESLint } from 'eslint';
 import path from 'path';
 
@@ -28,19 +27,34 @@ import path from 'path';
 // Classes
 // ============================================================================
 
+/**
+ * A class responsible for setting up and running linting tasks using ESLint.
+ * This class encapsulates all necessary configurations and methods to perform
+ * linting of TypeScript files within a given project root directory.
+ */
 class CodeLinter {
+
     private eslint: ESLint;
     private projectRoot: string;
 
+    /**
+     * Initializes a new instance of the CodeLinter class with a specified
+     * project root.
+     * @param projectRoot The root directory of the project where linting will
+     * be performed.
+     */
     constructor(projectRoot: string) {
         this.projectRoot = projectRoot;
         this.eslint = new ESLint({ cwd: projectRoot });
     }
 
     /**
-     * Runs ESLint on the specified files or directories.
+     * Runs ESLint on the specified files or directories and fixes fixable issues.
      * @param targetFiles Array of file or directory paths to lint.
-     * @returns A promise that resolves with the linting results.
+     * @returns A promise that resolves with the linting results, including
+     * any fixes applied.
+     * @throws An error if linting fails due to configuration issues or file
+     * reading problems.
      */
     async lintFiles(targetFiles: string[]): Promise<ESLint.LintResult[]> {
         try {
@@ -64,6 +78,11 @@ class CodeLinter {
 // ============================================================================
 
 export default CodeLinter;
+
+
+// ============================================================================
+// Example
+// ============================================================================
 
 // import CodeLinter from './CodeLinter';
 
