ac20a82c88d4e8b20a6b0dc1d29dbcfc3956503b
Lars van Vianen <lars@vianen.com>
2024-07-14 23:05:28 +0200

    Update postcss.config.compressed.ts

--- src/ts/config/postcss.config.compressed.ts
+++ src/ts/config/postcss.config.compressed.ts
@@ -19,23 +19,25 @@
 // Import
 // ============================================================================
 
-import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
+import autoprefixer from 'autoprefixer'; // Handles CSS vendor prefixing automatically
+import cssnano from 'cssnano'; // A PostCSS plugin for CSS minimization
 
 
 // ============================================================================
 // Constants
 // ============================================================================
 
+/**
+ * Configuration object for PostCSS that includes plugins for optimization and compression
+ * of CSS. This setup is typically used for production builds where minimized CSS is preferred
+ * to reduce file size and improve loading times.
+ */
 const postcssConfigCompressed = {
     plugins: [
-        autoprefixer,
-        // Minification for compressed output
-        cssnano(
-            {
-                preset: 'default'
-            },
-        ),
+        autoprefixer, // Automatically adds vendor prefixes to CSS rules
+        cssnano({     // Compresses CSS output
+            preset: 'default' // Uses the default settings for compression
+        }),
     ]
 };
 
