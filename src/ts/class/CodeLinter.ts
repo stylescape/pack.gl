4f95d9071153f37965fc6982367125b84195fe80
Lars van Vianen <lars@vianen.com>
2024-08-25 07:44:30 +0200

    Update CodeLinter.ts

--- src/ts/class/CodeLinter.ts
+++ src/ts/class/CodeLinter.ts
@@ -1,25 +1,10 @@
 // class/CodeLinter.ts
 
-// Copyright 2024 Scape Agency BV
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-// http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-
 // ============================================================================
 // Import
 // ============================================================================
 
-import { ESLint } from 'eslint';
+import { ESLint } from "eslint";
 
 
 // ============================================================================
@@ -39,6 +24,7 @@ class CodeLinter {
     /**
      * Initializes a new instance of the CodeLinter class with a specified
      * project root.
+     * 
      * @param projectRoot The root directory of the project where linting will
      * be performed.
      */
@@ -48,7 +34,9 @@ class CodeLinter {
     }
 
     /**
-     * Runs ESLint on the specified files or directories and fixes fixable issues.
+     * Runs ESLint on the specified files or directories and fixes fixable
+     * issues.
+     * 
      * @param targetFiles Array of file or directory paths to lint.
      * @returns A promise that resolves with the linting results, including
      * any fixes applied.
@@ -60,14 +48,14 @@ class CodeLinter {
             const results = await this.eslint.lintFiles(targetFiles);
             await ESLint.outputFixes(results);
 
-            const formatter = await this.eslint.loadFormatter('stylish');
+            const formatter = await this.eslint.loadFormatter("stylish");
             const resultText = formatter.format(results);
             console.log(resultText);
 
             return results;
 
         } catch (error) {
-            console.error('Error occurred while linting:', error);
+            console.error("Error occurred while linting:", error);
             throw error;
         }
     }
@@ -85,14 +73,14 @@ export default CodeLinter;
 // Example
 // ============================================================================
 
-// import CodeLinter from './CodeLinter';
+// import CodeLinter from "./CodeLinter";
 
 // const linter = new CodeLinter(process.cwd());
 
-// linter.lintFiles(['src/**/*.ts'])
+// linter.lintFiles(["src/**/*.ts"])
 //     .then(results => {
-//         console.log('Linting completed. Results:', results);
+//         console.log("Linting completed. Results:", results);
 //     })
 //     .catch(error => {
-//         console.error('Linting error:', error);
+//         console.error("Linting error:", error);
 //     });
