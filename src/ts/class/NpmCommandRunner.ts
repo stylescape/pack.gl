702f8bfd7eb9f03e004298f8d92e9b0f6455a3b3
Lars van Vianen <lars@vianen.com>
2024-08-25 07:44:51 +0200

    Update NpmCommandRunner.ts

--- src/ts/class/NpmCommandRunner.ts
+++ src/ts/class/NpmCommandRunner.ts
@@ -1,25 +1,11 @@
 // class/NpmCommandRunner.ts
 
-// Copyright 2024 Scape Agency BV
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-// http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 
 // ============================================================================
 // Import
 // ============================================================================
 
-import { exec } from 'child_process';
+import { exec } from "child_process";
 
 
 // ============================================================================
@@ -27,24 +13,25 @@ import { exec } from 'child_process';
 // ============================================================================
 
 /**
- * Provides a method to execute npm commands programmatically within a Node.js application.
- * This class is particularly useful for automating tasks that involve npm operations,
- * such as installing packages, running scripts, or updating dependencies.
+ * Provides a method to execute npm commands programmatically within a Node.js
+ * application. This class is particularly useful for automating tasks that
+ * involve npm operations, such as installing packages, running scripts, or
+ * updating dependencies.
  */
 class NpmCommandRunner {
 
     /**
      * Executes a given npm command and handles its output or errors.
      * 
-     * @param command The npm command to be executed, such as 'install', 'update', or a custom script.
+     * @param command The npm command to be executed, such as "install", "update", or a custom script.
      * @returns A promise that resolves with the standard output of the command, or rejects with an error message.
      * 
      * @example
      * // Example usage:
      * const runner = new NpmCommandRunner();
-     * runner.runCommand('install express')
-     *   .then(output => console.log('NPM Install Output:', output))
-     *   .catch(error => console.error('NPM Command Error:', error));
+     * runner.runCommand("install express")
+     *   .then(output => console.log("NPM Install Output:", output))
+     *   .catch(error => console.error("NPM Command Error:", error));
      */
     runCommand(command: string): Promise<string> {
         return new Promise((resolve, reject) => {
