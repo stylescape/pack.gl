0298b40fd963437bcdaab35b887ba1e01b617691
Lars van Vianen <lars@vianen.com>
2024-12-22 14:42:01 +0100

    Update index.ts

--- src/ts/interface/index.ts
+++ src/ts/interface/index.ts
@@ -2,9 +2,9 @@
 
 // Exporting all interfaces from the interfaces directory
 
-export { ActionInterface } from './ActionInterface';
-export { StepInterface } from './StepInterface';
-export { StepOptionsInterface } from './StepOptionsInterface';
-export { StageInterface } from './StageInterface';
-export { GlobalOptionsInterface } from './GlobalOptionsInterface';
-export { ConfigInterface } from './ConfigInterface';
+export { ActionInterface } from "./ActionInterface";
+export { StepInterface } from "./StepInterface";
+export { StepOptionsInterface } from "./StepOptionsInterface";
+export { StageInterface } from "./StageInterface";
+export { GlobalOptionsInterface } from "./GlobalOptionsInterface";
+export { ConfigInterface } from "./ConfigInterface";
