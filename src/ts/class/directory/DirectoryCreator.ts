d8f862ebbecc38d9bf3e7eae86f1f8b1eff65b52
Lars van Vianen <lars@vianen.com>
2024-12-22 10:49:35 +0100

    Update DirectoryCreator.ts

--- src/ts/class/directory/DirectoryCreator.ts
+++ src/ts/class/directory/DirectoryCreator.ts
@@ -18,19 +18,37 @@ import path from "path";
  */
  class DirectoryCreator {
 
+    // Parameters
+    // ========================================================================
+
+
+    // Constructor
+    // ========================================================================
+
+
+    // Methods
+    // ========================================================================
+
     /**
      * Asynchronously creates multiple directories based on a provided list of
      * paths. Directories are created within a specified base path. If
      * directories already exist, the operation skips those directories,
      * preventing any disruption of existing content.
      *
-     * @param basePath The base path where directories will be created, relative or absolute.
-     * @param directories An array of directory paths to create relative to the base path.
-     * @description Each directory is processed individually to ensure creation or validate existence.
-     *              This method uses the "recursive" option to create all necessary parent directories.
-     * @throws {Error} An error is thrown if there is a failure in creating any directory.
+     * @param basePath The base path where directories will be created,
+     * relative or absolute.
+     * @param directories An array of directory paths to create relative to
+     * the base path.
+     * @description Each directory is processed individually to ensure
+     * creation or validate existence. This method uses the "recursive"
+     * option to create all necessary parent directories.
+     * @throws {Error} An error is thrown if there is a failure in creating
+     * any directory.
      */
-    async createDirectories(basePath: string, directories: string[]): Promise<void> {
+    async createDirectories(
+        basePath: string,
+        directories: string[]
+    ): Promise<void> {
         try {
             for (const dir of directories) {
                 const dirPath = path.join(basePath, dir);
