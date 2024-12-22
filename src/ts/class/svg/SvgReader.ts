883c815b08c573ed2a8c0ace601bd2136e661a7c
Lars van Vianen <lars@vianen.com>
2024-12-22 10:50:23 +0100

    Update SvgReader.ts

--- src/ts/class/svg/SvgReader.ts
+++ src/ts/class/svg/SvgReader.ts
@@ -10,21 +10,34 @@ import { promises as fs } from "fs";
 // ============================================================================
 
 /**
- * Handles reading SVG files from the filesystem. This class can be used in applications
- * that need to process or analyze SVG content programmatically, such as in graphics
- * processing tools, web servers, or content management systems.
+ * Handles reading SVG files from the filesystem. This class can be used in
+ * applications that need to process or analyze SVG content programmatically,
+ * such as in graphics processing tools, web servers, or content management
+ * systems.
  */
 class SvgReader {
 
+    // Parameters
+    // ========================================================================
+
+
+    // Constructor
+    // ========================================================================
+
+
+    // Methods
+    // ========================================================================
+
     /**
      * Reads the content of an SVG file asynchronously.
-     * This method is useful for applications that need to load and manipulate SVG graphics,
-     * perhaps for rendering or further processing.
+     * This method is useful for applications that need to load and manipulate
+     * SVG graphics, perhaps for rendering or further processing.
      * 
      * @param filePath The path to the SVG file.
-     * @returns A promise that resolves to the content of the SVG file as a string.
-     * @throws Throws an error if the file cannot be read, which might occur due to issues
-     *         like file not existing or access permissions.
+     * @returns A promise that resolves to the content of the SVG file as a
+     * string.
+     * @throws Throws an error if the file cannot be read, which might occur
+     * due to issues like file not existing or access permissions.
      */
     async readSVG(filePath: string): Promise<string> {
         try {
