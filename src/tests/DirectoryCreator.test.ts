92d9d06ae46f7f472bc1f8a9deea7d99baec692a
Lars van Vianen <lars@vianen.com>
2024-12-22 10:51:01 +0100

    Update DirectoryCreator.test.ts

--- src/tests/DirectoryCreator.test.ts
+++ src/tests/DirectoryCreator.test.ts
@@ -1,37 +1,25 @@
 // tests/DirectoryCreator.test.ts
 
-// Copyright 2023 Scape Agency BV
 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-// http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 
 // ============================================================================
 // Import
 // ============================================================================
 
-import DirectoryCreator from '../ts/class/directory/DirectoryCreator';
-import fs from 'fs';
-import path from 'path';
+import DirectoryCreator from "../ts/class/directory/DirectoryCreator";
+import fs from "fs";
+import path from "path";
 
 
 // ============================================================================
 // Tests
 // ============================================================================
 
-describe('DirectoryCreator', () => {
+describe("DirectoryCreator", () => {
     const directoryCreator = new DirectoryCreator();
-    const basePath = './testDirectory';
-    const directories = ['dir1', 'dir2', 'dir3'];
+    const basePath = "./testDirectory";
+    const directories = ["dir1", "dir2", "dir3"];
 
     beforeAll(() => {
         // Optional: setup if required before all tests run
@@ -46,7 +34,7 @@ describe('DirectoryCreator', () => {
         fs.rmdirSync(basePath, { recursive: true });
     });
 
-    it('should create directories correctly', async () => {
+    it("should create directories correctly", async () => {
         await directoryCreator.createDirectories(basePath, directories);
 
         directories.forEach(dir => {
