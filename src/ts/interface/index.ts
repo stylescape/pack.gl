5d6b7637abacf2302f02cb066b8af173be4ed480
Lars van Vianen <lars@vianen.com>
2024-12-25 19:41:15 +0100

    Update index.ts

--- src/ts/interface/index.ts
+++ src/ts/interface/index.ts
@@ -6,5 +6,5 @@ export { ActionInterface } from "./ActionInterface";
 export { StepInterface } from "./StepInterface";
 export { StepOptionsInterface } from "./StepOptionsInterface";
 export { StageInterface } from "./StageInterface";
-export { GlobalOptionsInterface } from "./GlobalOptionsInterface";
+export { OptionsInterface } from "./OptionsInterface";
 export { ConfigInterface } from "./ConfigInterface";
