84808f26e85245347ba8e72da952ff744bba9430
Lars van Vianen <lars@vianen.com>
2025-02-02 14:34:50 +0100

    Update CoreActions.ts

--- src/ts/actions/CoreActions.ts
+++ src/ts/actions/CoreActions.ts
@@ -6,11 +6,28 @@ import { DirectoryCleanAction } from "../actions/DirectoryCleanAction";
 import { DirectoryCopyAction } from "../actions/DirectoryCopyAction";
 import { DirectoryCreateAction } from "../actions/DirectoryCreateAction";
 
+import { DocumentationAction } from "../actions/DocumentationAction";
+
+
 import { FileCopyAction } from "../actions/FileCopyAction";
+import { FileRenameAction } from "../actions/FileRenameAction";
+
+import { JavaScriptMinifyAction } from "../actions/JavaScriptMinifyAction";
+import { LintAction } from "../actions/LintAction";
+
 
 import { PackageManagerAction } from "../actions/PackageManagerAction";
 
 import { StyleProcessingAction } from "../actions/StyleProcessingAction";
+
+import { SvgPackagerAction } from "../actions/SvgPackagerAction";
+import { SvgReaderAction } from "../actions/SvgReaderAction";
+import { SvgSpriteAction } from "../actions/SvgSpriteAction";
+import { SvgToPngAction } from "../actions/SvgToPngAction";
+
+
+
+import { TypeScriptCompilerAction } from "../actions/TypeScriptCompilerAction";
 
 import { VersionWriteAction } from "../actions/VersionWriteAction";
 
@@ -28,11 +45,31 @@ import { ActionInterface } from "../interface/ActionInterface";
  * Automatically derives names from the action classes.
  */
 export const coreActions: Record<string, new () => ActionInterface> = {
+
     [new DirectoryCleanAction().name]: DirectoryCleanAction,
     [new DirectoryCopyAction().name]: DirectoryCopyAction,
     [new DirectoryCreateAction().name]: DirectoryCreateAction,
+
+    [new DocumentationAction().name]: DocumentationAction,
+
     [new FileCopyAction().name]: FileCopyAction,
+    [new FileRenameAction().name]: FileRenameAction,
+
+    [new JavaScriptMinifyAction().name]: JavaScriptMinifyAction,
+    [new LintAction().name]: LintAction,
+
     [new PackageManagerAction().name]: PackageManagerAction,
     [new StyleProcessingAction().name]: StyleProcessingAction,
+
+    [new SvgPackagerAction().name]: SvgPackagerAction,
+    [new SvgReaderAction().name]: SvgReaderAction,
+    [new SvgSpriteAction().name]: SvgSpriteAction,
+    [new SvgToPngAction().name]: SvgToPngAction,
+
+    [new TypeScriptCompilerAction().name]: TypeScriptCompilerAction,
+
+
+
     [new VersionWriteAction().name]: VersionWriteAction,
+
 };
