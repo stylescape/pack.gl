7024bbbb68a94c8b13ffed51b3a6abf388b072e0
Lars van Vianen <lars@vianen.com>
2025-05-10 09:57:41 +0200

    Update nunjucks.config.ts

--- src/ts/actions/TemplateRenderAction/nunjucks.config.ts
+++ src/ts/actions/TemplateRenderAction/nunjucks.config.ts
@@ -5,23 +5,28 @@
 // Import
 // ============================================================================
 
-// Importing path for potential future use in specifying template directories or other file paths
-import path from "node:path";
+// Importing path for potential future use in specifying template directories
+// or other file paths
 
 // ============================================================================
 // Constants
 // ============================================================================
 
 /**
- * Configuration options for Nunjucks to ensure safe and efficient template rendering.
- * This setup is ideal for both development and production environments, providing a balance
- * between performance optimizations and security best practices.
+ * Configuration options for Nunjucks to ensure safe and efficient template
+ * rendering. This setup is ideal for both development and production
+ * environments, providing a balance between performance optimizations and
+ * security best practices.
  */
 const nunjucksConfig = {
-    autoescape: true, // Controls if output with dangerous characters are escaped automatically
-    throwOnUndefined: false, // Throw errors when outputting a null/undefined value
-    trimBlocks: true, // Automatically remove trailing newlines from a block/tag
-    lstripBlocks: true, // Automatically remove leading whitespace from a block/tag
+    // Controls if output with dangerous characters are escaped automatically
+    autoescape: true,
+    // Throw errors when outputting a null/undefined value
+    throwOnUndefined: false,
+    // Automatically remove trailing newlines from a block/tag
+    trimBlocks: true,
+    // Automatically remove leading whitespace from a block/tag
+    lstripBlocks: true,
     noCache: true,
 };
 
